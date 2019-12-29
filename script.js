516334f7d2a75459ddbaabbda51f61ad35139d97
Rinat Nadav <ringhost@gmail.com>
2019-12-29 11:55:38 +0200

    yes

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 function hello(){
     console.log("hello world again");
-<<<<<<< HEAD
     console.log("new")
-=======
     console.log("this is a change");
 
->>>>>>> f2dc08eea36c259d93766fedba04f34d0d31c3ee
 }
 hello()
